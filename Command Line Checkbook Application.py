2d38be123282a91e80da2742736149cfc965530c
Matt Santos <mattsantos541@gmail.com>
2019-09-17 14:38:01 -0500

    second Commit

--- Command Line Checkbook Application.py
+++ Command Line Checkbook Application.py
@@ -1,1 +1,14 @@
 ##Command Line Checkbook Application
+name= input( "Welcome, may i get your name? ")
+print('Hello ' + str(name))
+print('\n                                   ')
+print ("What can I do for you today? ")
+print('\n')
+
+def options():
+  print("1. Current Balance")
+  print("2. Deposit")
+  print("3. Withdrawl")
+  print("4. End Session")
+
+  ######HELP
