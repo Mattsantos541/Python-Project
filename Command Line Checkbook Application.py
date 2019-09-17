884993152660fbab93dad6280f939646c8fe9ccb
Matt Santos <mattsantos541@gmail.com>
2019-09-17 15:09:41 -0500

    chpoice

--- Command Line Checkbook Application.py
+++ Command Line Checkbook Application.py
@@ -4,11 +4,12 @@ print('Hello ' + str(name))
 print('\n                                   ')
 print ("What can I do for you today? ")
 print('\n')
+print("1. Current Balance")
+print("2. Deposit")
+print("3. Withdrawl")
+print("4. End Session")
 
-def options():
-  print("1. Current Balance")
-  print("2. Deposit")
-  print("3. Withdrawl")
-  print("4. End Session")
-
-  ######HELP
+choice= int(input("please enter a number "))
+if choice > 4 or < 1:
+  print("Invalid Entry")
+######move on to options
