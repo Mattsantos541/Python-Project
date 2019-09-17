25f9629317fe69b75cc870a8a6b1f064a8d2e1c4
Matt Santos <mattsantos541@gmail.com>
2019-09-17 16:33:46 -0500

    still need to do error on entry

--- Command Line Checkbook Application.py
+++ Command Line Checkbook Application.py
@@ -1,24 +1,27 @@
 def current_balance():
   with open ("Master_account.txt" ,r) as MA:
-    transactions= MA.readlines()
+    transactions = MA.readlines()
     amount = 0
-    for line in transactions
-    amount+= line
+    for line in transactions:
+      amount+= line
     print("Your balance is ${0.2f}.".format.amount)
 
 def deposit():
-   deposit_amount= input("How much do you want to deposit? ")
+  deposit_amount= input("How much do you want to deposit? ")
     
-    with open ("Master_account.txt", r) as MA:
-      MA.write('\n')
-      MA.write(deposit_amount)
+  with open ("Master_account.txt", r) as MA:
+    MA.write('\n')
+    MA.write(deposit_amount)
 
 def Withdrawl():
   withdrawl_amount= input("How much do you want to withdrawl? ")
-    with open ("Master_account.txt", r) as MA:
-      MA.write('\n')
-      MA.write(withdrawl_amount)
-  
+  with open ("Master_account.txt", r) as MA:
+    MA.write('\n')
+    MA.write(withdrawl_amount)
+
+def user_input():
+  choice=input("please enter a number ")
+
 ##Command Line Checkbook Application
 name= input( "Welcome, may i get your name? ")
 print('Hello ' + str(name))
@@ -30,17 +33,19 @@ print("2. Deposit")
 print("3. Withdrawl")
 print("4. End Session")
 
-choice= int(input("please enter a number "))
-if choice > 4 or < 1:
-  print("Invalid Entry")
+user_input
+
+
+#if choice > 4 or < 1:
+  #print("Invalid Entry")
 ######move on to options
-elif choice== 1:
+if choice== 1:
   current_balance
 elif choice== 2:
   deposit
 elif choice== 3:
   Withdrawl
-else choice == 4:
-print("Thank You, Have a nice day")
+else:
+  print("Thank You, Have a nice day")
 
 
